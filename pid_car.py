d2f8dac9cc25c1605a4a7b9c9f18bf3083794dcc
Ivan <ivanwyf@foxmail.com>
2020-07-31 19:34:02 +0800

    modify something!

--- pid_car.py
+++ pid_car.py
@@ -4,7 +4,6 @@ import math
 import pygame as pg  # install
 from pygame.locals import *
 
-
 pg.init()
 size = width, height = 1200, 750  # 屏幕大小
 screen = pg.display.set_mode(size)
@@ -14,7 +13,7 @@ pg.display.set_caption("Car tracking")
 # move_car_rect=car_rect
 
 count = 0
-FPS = 60 # fps设定值
+FPS = 60  # fps设定值
 fps = 0
 start = time.time()
 clock = pg.time.Clock()
@@ -56,25 +55,25 @@ def get_gs_sensor_value(x, y, angle):
     step2 = 19
     step3 = 15
 
-    angle1 = math.pi*(angle+90)/180
-    angle = math.pi*(angle)/180
+    angle1 = math.pi * (angle + 90) / 180
+    angle = math.pi * (angle) / 180
 
-    x0 = x+step2*math.cos(angle1)+step3*math.cos(angle)
-    y0 = y-step2*math.sin(angle1)-step3*math.sin(angle)
+    x0 = x + step2 * math.cos(angle1) + step3 * math.cos(angle)
+    y0 = y - step2 * math.sin(angle1) - step3 * math.sin(angle)
 
     pg.draw.circle(screen, (255, 0, 0), (int(x0), int(y0)), 3, 1)  # 绘制0号灰度
 
-    x1 = x+step1*math.cos(angle1)+step3*math.cos(angle)
-    y1 = y-step1*math.sin(angle1)-step3*math.sin(angle)
+    x1 = x + step1 * math.cos(angle1) + step3 * math.cos(angle)
+    y1 = y - step1 * math.sin(angle1) - step3 * math.sin(angle)
     pg.draw.circle(screen, (255, 0, 0), (int(x1), int(y1)), 3, 1)  # 绘制1号灰度
 
-    x2, y2 = x+step3*math.cos(angle), y-step3*math.sin(angle)
+    x2, y2 = x + step3 * math.cos(angle), y - step3 * math.sin(angle)
     pg.draw.circle(screen, (255, 0, 0), (int(x2), int(y2)), 3, 1)  # 绘制2号（中间）灰度
 
-    x3 = x-step1*math.cos(angle1)+step3*math.cos(angle)
-    y3 = y+step1*math.sin(angle1)-step3*math.sin(angle)
-    x4 = x-step2*math.cos(angle1)+step3*math.cos(angle)
-    y4 = y+step2*math.sin(angle1)-step3*math.sin(angle)
+    x3 = x - step1 * math.cos(angle1) + step3 * math.cos(angle)
+    y3 = y + step1 * math.sin(angle1) - step3 * math.sin(angle)
+    x4 = x - step2 * math.cos(angle1) + step3 * math.cos(angle)
+    y4 = y + step2 * math.sin(angle1) - step3 * math.sin(angle)
     pg.draw.circle(screen, (255, 0, 0), (int(x3), int(y3)), 3, 1)  # 绘制3号灰度
     pg.draw.circle(screen, (255, 0, 0), (int(x4), int(y4)), 3, 1)  # 绘制4号灰度
 
@@ -86,10 +85,10 @@ def get_gs_sensor_value(x, y, angle):
         g3 = screen.get_at((int(x3), int(y3)))
         g4 = screen.get_at((int(x4), int(y4)))
     except:
-        g0, g1, g2, g3, g4 = (255, 255, 255, 255, 255)
+        g0, g1, g2, g3, g4 = (255, 255, 255, 255)
 
-    x5 = x+100*math.cos(angle)
-    y5 = y-100*math.sin(angle)
+    x5 = x + 100 * math.cos(angle)
+    y5 = y - 100 * math.sin(angle)
     pg.draw.line(screen, (255, 0, 0), (x, y), (x5, y5), 1)  # 绘制方向指示线
 
     return color_binary([g0, g1, g2, g3, g4])
@@ -101,22 +100,22 @@ def pid_angle(gs):
     global angle
     global last_error
     gs_black = 0  # 检测到黑色的灰度传感器数量
-    sum = 0
+    sum_ = 0
     for i in range(len(gs)):
         if gs[i] == 1:
             gs_black += 1
-            sum += i
+            sum_ += i
 
     if gs_black != 0:
-        sum = sum*10/gs_black-20
+        sum_ = sum_ * 10 / gs_black - 20
 
-    pid_p_out = kp*sum  # p
-    ed = sum-last_error  # 本次误差
-    last_error = sum  # 更新上次误差
-    pid_d_out = kd*ed  # d
+    pid_p_out = kp * sum_  # p
+    ed = sum_ - last_error  # 本次误差
+    last_error = sum_  # 更新上次误差
+    pid_d_out = kd * ed  # d
 
-    pid_out = pid_p_out+pid_d_out
-    angle = angle-pid_out
+    pid_out = pid_p_out + pid_d_out
+    angle = angle - pid_out
 
     if angle <= -180:
         angle = 180
@@ -125,9 +124,9 @@ def pid_angle(gs):
 def set_velocity(angle):
     # 设置速度矢量并更新x,y坐标
     global speed, velocity, x, y
-    angle = math.pi*(-angle)/180
-    velocity[0] = speed*math.cos(angle)
-    velocity[1] = speed*math.sin(angle)
+    angle = math.pi * (-angle) / 180
+    velocity[0] = speed * math.cos(angle)
+    velocity[1] = speed * math.sin(angle)
     x += velocity[0]
     y += velocity[1]
 
@@ -147,8 +146,8 @@ def set_velocity(angle):
 
 
 def main_loop():
-    # 主界面
-    global run, pause, start, fps, count, x, y, speed, angle
+    # 主循环
+    global run, pause, start, fps, count, x, y, angle
     while run:
         clock.tick(FPS)  # 设置fps
         for event in pg.event.get():
@@ -158,7 +157,6 @@ def main_loop():
                 if event.key == pg.K_p:  # P键暂停
                     pause = not pause
                 if event.key == pg.K_r:  # R键重新开始
-                    speed = 40
                     x, y = 555, 206
                     angle = 0
                     path.clear()
@@ -167,8 +165,7 @@ def main_loop():
         if pause:
             continue
 
-        # 鼠标坐标
-        mouse_pos_x, mouse_pos_y = pg.mouse.get_pos()
+        mouse_pos_x, mouse_pos_y = pg.mouse.get_pos()  # 鼠标坐标
 
         set_velocity(angle)  # 设置速度
 
@@ -184,20 +181,21 @@ def main_loop():
         # 计算fps
         count += 1
         now = time.time()
-        if now-start > 0.1:
-            fps = count/(now-start)
+        if now - start > 0.1:
+            fps = count / (now - start)
             start = now
             count = 0
-        fps_img = my_font.render('fps:'+str(round(fps, 5)), True, (0, 0, 255))
+
+        fps_img = my_font.render('fps:' + str(round(fps, 5)), True, (0, 0, 255))
 
         mouse_pos_img = my_font.render(
-            'pos_x:'+str(mouse_pos_x)+'  pos_y:'+str(mouse_pos_y), True, (0, 0, 255))
+            'pos_x:' + str(mouse_pos_x) + '  pos_y:' + str(mouse_pos_y), True, (0, 0, 255))
 
         para_img = my_font.render(
-            'kp:'+str(kp)+'  kd:'+str(kd)+'  speed:'+str(speed), True, (0, 0, 255))
+            'kp:' + str(kp) + '  kd:' + str(kd) + '  speed:' + str(speed), True, (0, 0, 255))
 
         about_img = my_font.render(
-            "press 'p' to pause, press 'r' to restart", True, (0, 0, 255))
+            "press 'p' to pause, press 'r' to restart!", True, (0, 0, 255))
         # 绘图
         screen.fill((255, 255, 255))  # 绘制白色背景
         screen.blit(fps_img, (10, 10))  # 绘制fps
@@ -211,7 +209,7 @@ def main_loop():
         pid_angle(gs)  # pid调速
 
         pg.draw.lines(screen, (255, 0, 0), 0, path, 1)  # 绘制轨迹
-        pg.display.flip()
+        pg.display.flip()  # 重新绘制窗口
 
     pg.quit()
 
